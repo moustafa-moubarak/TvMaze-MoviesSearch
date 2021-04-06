760ce2afb529901ae8e8f210ad86f210efaa5e5e
moustafa00 <moustafa.hisham00@gmail.com>
2021-04-06 17:47:17 +0200

    Style Changing and updated the MakeImages Function

--- app.js
+++ app.js
@@ -3,20 +3,31 @@ const photos = document.querySelector("#photos");
 
 searchForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  removeAllChildNodes(photos)
+  removeAllChildNodes(photos);
   searchTerm = searchForm.elements.search.value;
   const config = { params: { q: searchTerm } };
   result = await axios.get(`http://api.tvmaze.com/search/shows`, config);
   makeImages(result.data);
+  console.log(result.data);
 });
 
 const makeImages = (results) => {
   for (let image of results) {
     if (image.show.image) {
+      const div = document.createElement("DIV");
+      photos.append(div);
+      div.classList.add("imgContainer");
       const img = document.createElement("IMG");
       img.src = image.show.image.medium;
-      photos.append(img);
-      img.classList.add("img");
+      div.append(img);
+      // img.classList.add("img")
+      const info = document.createElement("P");
+
+      info.innerText = `Name: ${image.show.name}\n`;
+      if (image.show.rating.average) {
+        info.innerText += `\nAverage Rating: ${image.show.rating.average}`;
+      }
+      div.append(info);
     }
   }
 };
